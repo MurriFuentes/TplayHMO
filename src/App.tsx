342c2390271dd5fdd5b2199c0ca3674636f999f9
Christian Alberto <c.murrietafuentes@gmail.com>
2022-03-23 16:25:19 -0700

    Act

--- src/App.tsx
+++ src/App.tsx
@@ -28,10 +28,8 @@ class App extends React.Component {
     this.authenticate().then(() => {
       const ele = document.getElementById('ipl-progress-indicator')
       if(ele){
-        // fade out
         ele.classList.add('available')
         setTimeout(() => {
-          // remove from DOM
           ele.outerHTML = ''
         }, 300)
       }
