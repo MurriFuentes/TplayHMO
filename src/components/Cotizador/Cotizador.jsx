804f505ace2312dac1e55c95dd8f74cfa8806eeb
EDELAROCHA <edgar.delarocha@tress.com.mx>
2022-05-02 10:28:35 -0700

    Cambios en cotizador, agregar tabla de precios generada automaticamente

--- src/components/Cotizador/Cotizador.jsx
+++ src/components/Cotizador/Cotizador.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Cotizador.css";
 import {
   guardarCotizacion,
@@ -7,6 +7,7 @@ import {
 import Button from "react-bootstrap/Button";
 import { Form, Row, Col } from "react-bootstrap";
 import InnerSectionInfo from "./InnerSectionInfo";
+import TablaPrecios from './TablaPrecios'
 
 const initialState = {
   megasValue: "20",
@@ -29,15 +30,17 @@ export default function Cotizador() {
 
     let paquete = {
       velocidadInternet: formState.megasValue,
-      television: formState.televisionValue !== "0" ? true : false,
-      nuevoTotalPlayTv: formState.televisionValue === "1" ? true : false,
+      television: formState.televisionValue,
+      // nuevoTotalPlayTv: formState.televisionValue === "1" ? true : false,
       netflix: formState.streamingValue === "1" ? true : false,
       amazon: formState.streamingValue === "2" ? true : false,
-      cantidadPantallasNetflix: 1,
-      wifiExtender: formState.wifiExtenderValue,
-      tvAdicional: formState.tvExtraValue,
-      nuevoTotalPlayTvAdicional:
-        formState.televisionValue === "2" ? true : false,
+      // cantidadPantallasNetflix: 1,
+      wifiExtender: formState.wifiExtenderValue.toString(10),
+      tvAdicional: formState.tvExtraValue.toString(10),
+      hbo: formState.streamingValue === "1" ? true : false,
+      star: formState.streamingValue === "2" ? true : false,
+      // nuevoTotalPlayTvAdicional: 
+      //   formState.televisionValue === "2" ? true : false,
       canales140: formState.canalesValue === "1" ? true : false,
       canales230: formState.canalesValue === "2" ? true : false,
       canales280: formState.canalesValue === "3" ? true : false,
@@ -49,7 +52,7 @@ export default function Cotizador() {
     };
 
     if ((username === "admin") | (username === null)) {
-      guardarCotizacion(paquete, formState.telefono, formState.correo);
+      guardarCotizacion({ "paquete": paquete, "numeroTelefono": formState.telefono, "correoElectronico": formState.correo });
     } else {
       guardarCotizacionUsuario(
         paquete,
@@ -61,6 +64,14 @@ export default function Cotizador() {
 
     setFormState(initialState);
   };
+
+  useEffect(() => {
+    setFormState((prev) => ({
+      ...prev,
+      televisionValue: "1",
+    }));
+  }, [])
+  
 
   const onChange = (event) => {
     setFormState((prev) => ({
@@ -100,6 +111,12 @@ export default function Cotizador() {
   const toggleTab = (index) => {
     setToggleState(index);
     setFormState(initialState);
+    if (index == 1) {
+      setFormState((prev) => ({
+        ...prev,
+        televisionValue: "1",
+      }));
+    }
   };
 
   const Canales = [
@@ -134,173 +151,188 @@ export default function Cotizador() {
 
   return (
     <>
-      <div className="cotizador_container">
-        <div className="bloc-tabs">
-          <button
-            className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(1)}
-          >
-            Triple Play
-          </button>
-          <button
-            className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(2)}
-          >
-            Doble Play
-          </button>
-        </div>
-
-        <div className="content-tabs">
-          <div className="content  active-content">
-            <div className="tab_infoContainer">
-              <InnerSectionInfo
-                onChange={onChange}
-                formState={formState}
-                data={MegasDisp}
-                title="Elige tu velocidad de internet"
-                name="megasValue"
-                toggleState={1}
-              />
-            </div>
-
-            <div className="vertical">
-              <InnerSectionInfo
-                onChange={onChange}
-                formState={formState}
-                data={Premium}
-                title="¿TV Premium?"
-                name="tvPremiumValue"
-                toggleState={toggleState}
-              />
-
-              <InnerSectionInfo
-                onChange={onChange}
-                formState={formState}
-                data={Television}
-                title="Television"
-                name="televisionValue"
-                toggleState={toggleState}
-              />
-            </div>
-
-            <div className="vertical">
-              <InnerSectionInfo
-                onChange={onChange}
-                formState={formState}
-                data={Streaming}
-                title="¿Servicio de Streaming?"
-                name="streamingValue"
-                toggleState={1}
-              />
-
-              <InnerSectionInfo
-                onChange={onChange}
-                formState={formState}
-                data={Canales}
-                title="¿Mas canales?"
-                name="canalesValue"
-                toggleState={toggleState}
-              />
-            </div>
-
-            <div className="Row">
-              <div className="tab_InnerSection">
-                <h5>¿Wifi Extender?</h5>
-                <div className="button_container">
-                  <Button
-                    variant="primary"
-                    onClick={onDecrementWifi}
-                    disabled={formState.wifiExtenderValue <= 0 ? true : false}
-                  >
-                    -
-                  </Button>
-                  <h3>{formState.wifiExtenderValue}</h3>
-                  <Button
-                    variant="primary"
-                    onClick={onIncrementWifi}
-                    disabled={formState.wifiExtenderValue >= 5 ? true : false}
-                  >
-                    +
-                  </Button>
-                </div>
-              </div>
-              <div
-                className={
-                  "tab_InnerSection " + (toggleState === 2 ? "disabled" : "")
-                }
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <div className="col-6">
+            <TablaPrecios formState={formState} />
+          </div>
+          <div className="cotizador_container col-6">
+            <div className="bloc-tabs">
+              <button
+                className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+                onClick={() => toggleTab(1)}
               >
-                <h5>¿Television Extra?</h5>
-                <div className="button_container">
-                  <Button
-                    variant="primary"
-                    onClick={onDecrementTvExtra}
-                    disabled={
-                      formState.tvExtraValue <= 0
-                        ? true
-                        : false || toggleState === 2
-                    }
-                  >
-                    -
-                  </Button>
-                  <h3>{formState.tvExtraValue}</h3>
-                  <Button
-                    variant="primary"
-                    onClick={onIncrementTvExtra}
-                    disabled={
-                      formState.tvExtraValue >= 5
-                        ? true
-                        : false || toggleState === 2
-                    }
-                  >
-                    +
-                  </Button>
-                </div>
-              </div>
+                Triple Play
+              </button>
+              <button
+                className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+                onClick={() => toggleTab(2)}
+              >
+                Doble Play
+              </button>
             </div>
-            <div className="tab_infoContainer">
-              <div className="tab_InnerSection">
-                <Form onSubmit={handleSubmit}>
-                  <Row>
-                    <Col>
-                      <Form.Group className="mb-3">
-                        <Form.Label></Form.Label>
-                        <Form.Control
-                          required
-                          type="email"
-                          name="correo"
-                          minLength={10}
-                          placeholder="Correo (Opcional)"
-                          value={formState.correo}
-                          onChange={onChange}
-                        />
-                      </Form.Group>
-                    </Col>
-                    <Col>
-                      <Form.Group className="mb-3">
-                        <Form.Label></Form.Label>
-                        <Form.Control
-                          onKeyPress={(event) => {
-                            if (!/[0-9]/.test(event.key)) {
-                              event.preventDefault();
-                            }
-                          }}
-                          maxLength={10}
-                          minLength={10}
-                          required
-                          name="telefono"
-                          placeholder="Telefono (Opcional)"
-                          value={formState.telefono}
-                          onChange={onChange}
-                        />
-                      </Form.Group>
-                    </Col>
-                    <Col className="vertical">
-                      <Button variant="primary" type="submit" size="lg">
-                        Cotizar
+
+            <div className="content-tabs">
+              <div className="content active-content">
+                <div className="tab_infoContainer">
+                  <InnerSectionInfo
+                    onChange={onChange}
+                    formState={formState}
+                    data={MegasDisp}
+                    title="Elige tu velocidad de internet"
+                    name="megasValue"
+                    toggleState={1}
+                  />
+                </div>
+
+                <div className="vertical">
+                  <InnerSectionInfo
+                    onChange={onChange}
+                    formState={formState}
+                    data={Premium}
+                    title="¿TV Premium?"
+                    name="tvPremiumValue"
+                    toggleState={toggleState}
+                  />
+
+                  <InnerSectionInfo
+                    onChange={onChange}
+                    formState={formState}
+                    data={Television}
+                    title="Television"
+                    name="televisionValue"
+                    toggleState={toggleState}
+                  />
+                </div>
+
+                <div className="vertical">
+                  <InnerSectionInfo
+                    onChange={onChange}
+                    formState={formState}
+                    data={Streaming}
+                    title="¿Servicio de Streaming?"
+                    name="streamingValue"
+                    toggleState={1}
+                  />
+
+                  <InnerSectionInfo
+                    onChange={onChange}
+                    formState={formState}
+                    data={Canales}
+                    title="¿Mas canales?"
+                    name="canalesValue"
+                    toggleState={toggleState}
+                  />
+                </div>
+
+                <div className="Row">
+                  <div className="tab_InnerSection">
+                    <h5>¿Wifi Extender?</h5>
+                    <div className="button_container">
+                      <Button
+                        variant="primary"
+                        onClick={onDecrementWifi}
+                        disabled={formState.wifiExtenderValue <= 0 ? true : false}
+                      >
+                        -
                       </Button>
-                    </Col>
-                  </Row>
-                </Form>
+                      <h3>{formState.wifiExtenderValue}</h3>
+                      <Button
+                        variant="primary"
+                        onClick={onIncrementWifi}
+                        disabled={formState.wifiExtenderValue >= 4 ? true : false}
+                      >
+                        +
+                      </Button>
+                    </div>
+                  </div>
+                  <div
+                    className={
+                      "tab_InnerSection " + (toggleState === 2 ? "disabled" : "")
+                    }
+                  >
+                    <h5>¿Television Extra?</h5>
+                    <div className="button_container">
+                      <Button
+                        variant="primary"
+                        onClick={onDecrementTvExtra}
+                        disabled={
+                          formState.tvExtraValue <= 0
+                            ? true
+                            : false || toggleState === 2
+                        }
+                      >
+                        -
+                      </Button>
+                      <h3>{formState.tvExtraValue}</h3>
+                      <Button
+                        variant="primary"
+                        onClick={onIncrementTvExtra}
+                        disabled={
+                          formState.tvExtraValue >= 4
+                            ? true
+                            : false || toggleState === 2
+                        }
+                      >
+                        +
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+                <div className="tab_infoContainer">
+                  <div className="tab_InnerSection">
+                    <Form onSubmit={handleSubmit}>
+                      <Row>
+                        <Col className="col-6">
+                          <Form.Group className="mb-3">
+                            <Form.Label></Form.Label>
+                            <Form.Control
+                              required
+                              type="email"
+                              name="correo"
+                              minLength={10}
+                              placeholder="Correo"
+                              value={formState.correo}
+                              onChange={onChange}
+                            />
+                          </Form.Group>
+                        </Col>
+                        <Col className="col-6">
+                          <Form.Group className="mb-3">
+                            <Form.Label></Form.Label>
+                            <Form.Control
+                              onKeyPress={(event) => {
+                                if (!/[0-9]/.test(event.key)) {
+                                  event.preventDefault();
+                                }
+                              }}
+                              maxLength={10}
+                              minLength={10}
+                              required
+                              name="telefono"
+                              placeholder="Telefono"
+                              value={formState.telefono}
+                              onChange={onChange}
+                            />
+                          </Form.Group>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col className="col-6">
+                          <Button variant="primary vertical" type="submit" size="lg">
+                            Cotizar
+                          </Button>
+
+                        </Col>
+                        <Col className="col-6">
+                          <Button variant="primary vertical" onClick={() => setFormState(initialState)} size="lg">
+                            Reiniciar
+                          </Button>
+                        </Col>
+                      </Row>
+                    </Form>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
--- src/pages/Cotizar.jsx
+++ src/pages/Cotizar.jsx
@@ -1,8 +1,9 @@
 import Cotizador from "../components/Cotizador/Cotizador";
 export default function Page_Cotizador() {
-    return(
-        <div className="Section Row">
-                <div className="Cot_Message_Container">
+    return (
+        <>
+            <div className="container mt-5">
+                <div className="Cot_Message_Container p-4 text-center">
                     <h2>
                         Modela el paquete de tu preferencia
                     </h2>
@@ -11,7 +12,10 @@ export default function Page_Cotizador() {
                         pesos en la instalacion
                     </p>
                 </div>
-                <Cotizador/>
-        </div>
+            </div>
+            <div className="container-fluid">
+                <Cotizador />
+            </div>
+        </>
     );
 }
